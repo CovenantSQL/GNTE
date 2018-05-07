f768f9f45e6056509ab978922e14e627cb320eea
laodouya <laodouya@yeah.net>
2018-05-07 21:06:19 +0800

    change to yaml definition.

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo docker run -dit --rm -v $DIR/scripts:/scripts --cap-add=NET_ADMIN ns
+sudo docker run -dit --rm -v $DIR/scripts:/scripts -v $DIR/usage:/usage --cap-add=NET_ADMIN ns
