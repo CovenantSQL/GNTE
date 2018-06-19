40a424a85d9d2dbf9689338be2ac64e18ce86f42
laodouya <laodouya@yeah.net>
2018-06-19 00:01:37 -0700

    modify dir struct

--- launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo docker run -dit --rm -v $DIR/scripts:/scripts -v $DIR/usage:/usage --cap-add=NET_ADMIN ns
--- run.md
+++
@@ -1,2 +0,0 @@
-docker run --name client --rm -it --cap-add=NET_ADMIN ns
-
