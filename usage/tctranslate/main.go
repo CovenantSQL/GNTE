7c355a815ba044dd9346f3f820667a507a6d2d30
laodouya <laodouya@yeah.net>
2018-06-18 23:44:27 -0700

    generage .gv file

--- usage/tctranslate/main.go
+++ usage/tctranslate/main.go
@@ -123,7 +123,7 @@ func printTcScript(rules []string, node string) {
 	data = []byte(rulestr + "\n")
 
 	//	fmt.Println(rulestr)
-	err := ioutil.WriteFile("/home/laodouya/thunderdb/ns/scripts/"+ip+".sh", data, 0777)
+	err := ioutil.WriteFile(ip+".sh", data, 0777)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -132,7 +132,7 @@ func printTcScript(rules []string, node string) {
 func printDockerScript(r root) {
 
 	initFile, err := os.OpenFile(
-		"/home/laodouya/thunderdb/ns/init.sh",
+		"init.sh",
 		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
 		0777,
 	)
@@ -142,7 +142,7 @@ func printDockerScript(r root) {
 	defer initFile.Close()
 
 	lanuchFile, err := os.OpenFile(
-		"/home/laodouya/thunderdb/ns/launch.sh",
+		"launch.sh",
 		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
 		0777,
 	)
@@ -159,9 +159,9 @@ func printDockerScript(r root) {
 	for _, group := range r.Group {
 		for _, node := range group.Nodes {
 			ip := strings.Split(node, "/")[0]
-			initFileData = append(initFileData, "docker network create --subnet="+node+" "+ip+"\n")
+			initFileData = append(initFileData, "docker network create --subnet="+node+" "+ip)
 
-			lanuchFileData = append(lanuchFileData, "sudo docker run -dit --rm --net "+ip+" --ip  "+ip+" -v $DIR/scripts:/scripts --cap-add=NET_ADMIN ns /scripts/" + ip + ".sh")
+			lanuchFileData = append(lanuchFileData, "sudo docker run -dit --rm --net "+ip+" --ip  "+ip+" -v $DIR/scripts:/scripts --cap-add=NET_ADMIN ns /scripts/"+ip+".sh")
 		}
 	}
 
@@ -178,6 +178,68 @@ func printDockerScript(r root) {
 
 }
 
+func printGraphScript(r root) {
+
+	gvFile, err := os.OpenFile(
+		"graph.gv",
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+		0666,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gvFile.Close()
+
+	var gvFileData []string
+	gvFileData = append(gvFileData, "digraph G {")
+	gvFileData = append(gvFileData, "    compound=true;")
+
+	for _, group := range r.Group {
+		gvFileData = append(gvFileData, "    subgraph cluster_"+group.Name+" {")
+		gvFileData = append(gvFileData, "        label = "+group.Name+";")
+		gvFileData = append(gvFileData, "        style = rounded;")
+		for i := 0; i < len(group.Nodes); i++ {
+			for j := i + 1; j < len(group.Nodes); j++ {
+				nodei := group.Nodes[i]
+				nodej := group.Nodes[j]
+				//"10.2.1.1/16" -> "10.3.1.1/20" [arrowhead=none, arrowtail=none, label="delay\n 100ms ±10ms 30%"];
+				gvFileData = append(gvFileData, "        \""+nodei+"\" -> \""+nodej+
+					"\" [arrowhead=none, arrowtail=none, label=\"delay "+
+					group.Delay+"\"];")
+			}
+		}
+		gvFileData = append(gvFileData, "    }")
+	}
+
+	for _, network := range r.Network {
+		//parse two group pair
+		for i := 0; i < len(network.Groups); i++ {
+			for j := i + 1; j < len(network.Groups); j++ {
+				var groupNodei, groupNodej string
+				// get group ip
+				for _, group := range r.Group {
+					if group.Name == network.Groups[i] {
+						groupNodei = group.Nodes[0]
+					} else if group.Name == network.Groups[j] {
+						groupNodej = group.Nodes[0]
+					}
+				}
+				gvFileData = append(gvFileData, "    \""+groupNodei+"\" -> \""+groupNodej+
+					"\"\n        [ltail=cluster_"+network.Groups[i]+", lhead=cluster_"+network.Groups[j]+
+					", arrowhead=none, arrowtail=none,\n        label=\"delay "+
+					network.Delay+"\"];")
+			}
+		}
+	}
+
+	gvFileData = append(gvFileData, "}")
+	gvFileByte := []byte(strings.Join(gvFileData, "\n") + "\n")
+	_, err = gvFile.Write(gvFileByte)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 /*
 1. read yaml from file
 2. select one node
@@ -188,12 +250,11 @@ func printDockerScript(r root) {
 	3.4 build tc leaf for group-connection
 4. print tc tree
 */
-//TODO 1. read yaml from specific file
-// 2. write to running dir
 func main() {
 	r := root{}
 	//1. read yaml from file
-	data, err := ioutil.ReadFile("/home/laodouya/thunderdb/ns/usage/example.yaml")
+	//TODO 1. read yaml from specific file
+	data, err := ioutil.ReadFile("example.yaml")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -209,8 +270,11 @@ func main() {
 			tcRules := processOneNode(node, group.Name, r)
 			//4. print tc tree
 			printTcScript(tcRules, node)
-			//	printRules(tcRules)
 		}
 	}
 	printDockerScript(r)
+
+	printGraphScript(r)
+	//TODO 2. check dot commandline
+	// dot -Tpng graph.gv -o graph.png
 }
